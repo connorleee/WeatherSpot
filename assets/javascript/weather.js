edbd94d021a29c4f4b3209690bd4dd98f8a348ec
Lilian X <sbhgn0721@gmail.com>
2019-04-11 20:05:30 -0700

    add search button id

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -1,3 +1,18 @@
 function displayWeather() {
+    var APIKey = "78c022ae7b87430bbaabb56f3fd651a0";
+    var cityName;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + APIKey;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    }).then(function (response){
+
+        console.log(queryURL)
+        console.log(response);
+    })
     
 }
+
+//Adding a click event listener to search button
+$(document).on("click")
