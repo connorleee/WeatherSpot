ae5a727bd38d7bd9027c8e7e38853f44ae1adbb2
Lilian X <sbhgn0721@gmail.com>
2019-04-13 00:39:21 -0700

    completed input city name to get weather data, and finished weather.js file

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -27,77 +27,11 @@ $(":checkbox").on("click", function getLocation() {
                         url: queryURLGeolocation,
                         method: "GET",
                     }).then(function (weather) {
-                        console.log(queryURLGeolocation);
-                        console.log(weather)
-                        //retrieve country name and city name
-                        var countryNameByGeolocation = weather.sys.country;
-                        console.log(countryNameByGeolocation);
-                        var cityNameByGeolocation = weather.name;
-                        console.log(cityNameByGeolocation);
-
-                        //combine city name and country
-                        var pOne = $("<p>").text("Weathher in " + cityNameByGeolocation + ", " + countryNameByGeolocation);
-                        
-
-
-
-                        //retrieve weather icon
-                        var weatherIconID = weather.weather[0].icon;
-                        console.log(weatherIconID);
-                        var weatherIconURL = "http://openweathermap.org/img/w/" + weatherIconID + ".png";
-                        console.log(weatherIconURL);
-                        var weatherIconImage = $("<img>").attr("src", weatherIconURL)
-                        console.log(weatherIconImage);
-
-                        //retrieve temperature
-                        var currentTemp = weather.main.temp;
-                        console.log(currentTemp);
-
-                        //combine weather icon and main weather condition
-                        var pTwo = weatherIconImage + currentTemp
-
-                        //retrieve main weather condition
-                        var weatherMain = weather.weather[0].main;
-                        console.log(weatherMain);
-                        var pThree = weatherMain;
-
-                        //Show current time and data
-                        var currentTime = moment().format("MMM Do YYYY, hh:mm A")
-                        console.log(currentTime);
-                        var pTime = currentTime;
-
-                        //retrieve clouds data
-                        var cloudiness = weather.clouds.all;
-                        console.log(cloudiness);
-                        var pFour = "Cloudiness: " + cloudiness + "%";
-
-                        //retrieve humudity
-                        var humidity = weather.main.humidity;
-                        console.log(humidity);
-                        var pFive = "Humidity: "+ humidity + "%";
-
-                        //retrieve sunrise data
-                        var sunrise = weather.sys.sunrise;
-                        var sunrisePrettify = moment(sunrise, "X").format("HH:mm");
-                        console.log(sunrisePrettify);
-                        var pSix = "Sunrise: " + sunrisePrettify;
-
-                        //retrieve sunset data
-                        var sunset = weather.sys.sunset;
-                        var sunsetPrettify = moment(sunset, "X").format("HH:mm");
-                        console.log(sunsetPrettify);
-                        var pSeven = "Sunset: " + sunsetPrettify;
-
-                        //Put the above weather data into html
-                        
-
-                      
+                    displayWeatherData(weather);
 
                     })
                 }
                 displayWeatherByGeolocation();
-                
-                
             },
 
             function error(error_message) {
@@ -114,42 +48,101 @@ $(":checkbox").on("click", function getLocation() {
 })
 
 
+//Define function displayWeatherData to retrieve weather data from weather api and display weather data in html
+
+function displayWeatherData(weather) {
+    //console.log(queryURLGeolocation);
+    console.log(weather)
+    //retrieve country name and city name
+    var countryNameByGeolocation = weather.sys.country;
+    console.log(countryNameByGeolocation);
+    var cityNameByGeolocation = weather.name;
+    console.log(cityNameByGeolocation);
+
+    //combine city name and country
+    var cityAndCountry = cityNameByGeolocation + ", " + countryNameByGeolocation;
+
+    //put city name and country into html
+    $("#currentLocation").html(cityAndCountry);
 
 
-//Show the user's city on the weather image
+    //retrieve weather icon
+    var weatherIconID = weather.weather[0].icon;
+    console.log(weatherIconID);
+    var weatherIconURL = "http://openweathermap.org/img/w/" + weatherIconID + ".png";
+    console.log(weatherIconURL);
+    var weatherIconImage = $("<img>").attr("src", weatherIconURL)
+    console.log(weatherIconImage);
 
-//Show current time and date
+    //retrieve temperature
+    var currentTemp = weather.main.temp;
+    console.log(currentTemp);
+    var pCurrentTemp = $("<p>").text(currentTemp + " Fahrenheit");
 
-//Show the current weather data including general weather condition, temperature, humidity, wind, cloudiness, humidity, sunrise and sunset
 
 
+    //retrieve main weather conditio
+    var weatherMain = weather.weather[0].main;
+    console.log(weatherMain);
+    var pWeatherMain = $("<p>").text(weatherMain);
+
+
+    //Show current time and data
+    var currentTime = moment().format("MMM Do YYYY, hh:mm A")
+    console.log(currentTime);
+    var pCurrentTime = $("<p>").text(currentTime);
+
+
+    //retrieve clouds data
+    var cloudiness = weather.clouds.all;
+    console.log(cloudiness);
+    var pCloudiness = $("<p>").text("Cloudiness: " + cloudiness + "%");
+
+    //retrieve humudity
+    var humidity = weather.main.humidity;
+    console.log(humidity);
+    var pHumidity = $("<p>").text("Humidity: " + humidity + "%");
+
+    //retrieve sunrise data
+    var sunrise = weather.sys.sunrise;
+    var sunrisePrettify = moment(sunrise, "X").format("HH:mm");
+    console.log(sunrisePrettify);
+    var pSunrise = $("<p>").text("Sunrise: " + sunrisePrettify);
+
+    //retrieve sunset data
+    var sunset = weather.sys.sunset;
+    var sunsetPrettify = moment(sunset, "X").format("HH:mm");
+    console.log(sunsetPrettify);
+    var pSunset = $("<p>").text("Sunset: " + sunsetPrettify);
+
+    //Put the above weather data into html
+    $(".weatherMain").empty().append(
+        weatherIconImage,
+        pCurrentTime,
+        pWeatherMain,
+        pCurrentTemp,
+        pCloudiness,
+        pHumidity,
+        pSunrise,
+        pSunset
+
+    )
+}
 
 //If user doesn't check "Allow this page to use your location checkbox"， user needs to input the city name or zip code(doesn't allow to submit empty form)
-
-//Show the user's city on the weather image
-
-//Show current time and date
-
-//Show the current weather data including general weather condition, temperature, humidity, wind, cloudiness, humidity, sunrise and sunset
-
-
-
 
 function displayWeatherByCity() {
     var APIKey = "78c022ae7b87430bbaabb56f3fd651a0";
     var cityName = $(".location").val().trim();
+
     var queryURLCity = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey + "&units=imperial";
 
     $.ajax({
         url: queryURLCity,
         method: "GET",
-    }).then(function (response) {
-
-        console.log(queryURL)
-        console.log(response);
-
+    }).then(function (weather) {
+        displayWeatherData(weather);
     })
-
 }
 
 //Adding a click event listener to search button
