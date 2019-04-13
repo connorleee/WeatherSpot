4910c257b87116d807da2b323ccbcc913f6f83a9
Connor Lee <45024235+connorleee@users.noreply.github.com>
2019-04-13 11:59:04 -0700

    Got spotify api call to work using Spotify's console in their documentation. will have to have a list of Spotify playlist id's

---
+++ assets/javascript/spotify.js
@@ -0,0 +1,18 @@
+
+
+var playlistID = "37i9dQZF1DX7KNKjOK0o75"
+var queryURL = "https://api.spotify.com/v1/playlists/" + playlistID
+
+
+$.ajax({
+    url: queryURL,
+    headers: {
+        "Accept" : "application/json",
+        "Content-Type" : "application/json",
+        "Authorization" : "Bearer BQBmORlZpHzLwGCBqT0rcqFnEOK1OLnk6rHv_XTrpHbpRNYYIK4sgNro9br8pnm111mrQvwL24N8Lc6PG8OE__ZP5lCUn59bvTDILTaEkeBbaYLmQgHgfT9s_mK-svqAJSQZfzpg6uHqBDMRWw"
+    }, 
+    method: "GET"
+
+}).then(function (response) {
+    console.log(response);
+})
